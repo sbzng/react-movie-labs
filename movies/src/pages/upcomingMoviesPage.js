d476ee1440ae78d89150b74ac41169d73c0f29bc
Shaobo Zang <20105527@mail.wit.ie>
2023-11-03 20:45:34 +0000

    Change upcoming movies page to use caching.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
 
 const UpcomingMoviesPage = () => {
-    const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-        getUpcomingMovies().then((movies) => {
-            setMovies(movies);
-        });
-    }, []);
+    const { data, error, isLoading, isError } = useQuery('discover', getUpcomingMovies)
+    if (isLoading) {
+        return <Spinner />
+    }
+    if (isError) {
+        return <h1>{error.message}</h1>;
+    }
+    const movies = data.results;
 
     return (
         <PageTemplate
@@ -18,7 +21,7 @@ const UpcomingMoviesPage = () => {
             movies={movies}
             action={(movie) => {
                 return <PlaylistAddIcon movie={movie} />
-              }}
+            }}
         />
     );
 };
