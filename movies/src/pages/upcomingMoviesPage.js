674141b4704e908d14f6234e807b66404734e3df
Shaobo Zang <20105527@mail.wit.ie>
2023-11-03 18:09:49 +0000

    Fix upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
 const UpcomingMoviesPage = () => {
     const [movies, setMovies] = useState([]);
@@ -24,7 +25,9 @@ const UpcomingMoviesPage = () => {
         <PageTemplate
             title="Upcoming Movies"
             movies={movies}
-            selectFavorite={addToFavorites}
+            action={(movie) => {
+                return <AddToFavoritesIcon movie={movie} />
+              }}
         />
     );
 };
