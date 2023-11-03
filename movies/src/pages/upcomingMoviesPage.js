d157a2c2a3f4517c81f48e0ac7d2fefbf2035f22
Shaobo Zang <20105527@mail.wit.ie>
2023-11-03 20:27:40 +0000

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,19 +1,10 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 
 const UpcomingMoviesPage = () => {
     const [movies, setMovies] = useState([]);
-
-    const favorites = movies.filter((m) => m.favorite);
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-    const addToFavorites = (movieId) => {
-        const updatedMovies = movies.map((m) =>
-            m.id === movieId ? { ...m, favorite: true } : m
-        );
-        setMovies(updatedMovies);
-    };
 
     useEffect(() => {
         getUpcomingMovies().then((movies) => {
@@ -26,7 +17,7 @@ const UpcomingMoviesPage = () => {
             title="Upcoming Movies"
             movies={movies}
             action={(movie) => {
-                return <AddToFavoritesIcon movie={movie} />
+                return <PlaylistAddIcon movie={movie} />
               }}
         />
     );
